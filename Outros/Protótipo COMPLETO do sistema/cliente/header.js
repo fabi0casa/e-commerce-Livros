af2c66fce38898f6522f3defb4b401699b8daa5a
fabin0casa <fabiocasa999@gmail.com>
2025-03-03 23:22:55 -0300

    fazendo protótipo da busca de livros

--- Outros/Protótipo COMPLETO do sistema/cliente/header.js
+++ Outros/Protótipo COMPLETO do sistema/cliente/header.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const header = `
         <header>
             <a href="home.html" class="logo"></a>
-            <input type="text" class="search-bar" placeholder="Buscar livros...">
+            <input type="text" class="search-bar" placeholder="Buscar livros..." id="searchInput">
             <div class="header-buttons">
                 <button class="btn">
                     <img src="css/notification.png" alt="Notificações">
@@ -21,4 +21,13 @@ document.addEventListener("DOMContentLoaded", function() {
     `;
     
     document.body.insertAdjacentHTML("afterbegin", header);
+
+    //função pra redirecionar no input de busca
+    document.getElementById("searchInput").addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            let query = encodeURIComponent(this.value); // Evita erros com espaços ou caracteres especiais
+            window.location.href = "buscar.html?q=" + query;
+        }
+    });
 });
