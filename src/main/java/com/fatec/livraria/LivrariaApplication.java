c288cc19ca688141cd643f693d9d148fb23f0014
fabin0casa <fabiocasa999@gmail.com>
2025-03-06 18:30:14 -0300

    carregando as variáveis de ambiente antes da inicialização do programa

--- src/main/java/com/fatec/livraria/LivrariaApplication.java
+++ src/main/java/com/fatec/livraria/LivrariaApplication.java
@@ -3,8 +3,18 @@ package com.fatec.livraria;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class LivrariaApplication {
+
+        static {
+            // Carregando variáveis de ambiente antes da inicialização do programa
+            Dotenv dotenv = Dotenv.load();
+            System.setProperty("DB_URL", dotenv.get("DB_URL"));
+            System.setProperty("DB_USER", dotenv.get("DB_USER"));
+            System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        }
 
 	public static void main(String[] args) {
 		SpringApplication.run(LivrariaApplication.class, args);
