70e05900170797c52cc845e7f94e814b610bc9a5
fabin0casa <fabiocasa999@gmail.com>
2025-05-26 15:01:33 -0300

    feat: carregando variável de ambiente para o projeto

--- src/main/java/com/fatec/livraria/LivrariaApplication.java
+++ src/main/java/com/fatec/livraria/LivrariaApplication.java
@@ -4,11 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @ComponentScan(basePackages = "com.fatec.livraria")
 public class LivrariaApplication {
 
 	public static void main(String[] args) {
+		
+		//carrega as variáveis do .env
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("GEMINI_API_KEY", dotenv.get("GEMINI_API_KEY"));
+
 		SpringApplication.run(LivrariaApplication.class, args);
 	}
 
