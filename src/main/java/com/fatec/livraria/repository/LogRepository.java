66b4cd80b22c3aaf8b638f7789fcbedffd8e68a1
fabin0casa <fabiocasa999@gmail.com>
2025-06-11 18:47:13 -0300

    fix: desistindo da modelagem antiga do logs

--- src/main/java/com/fatec/livraria/repository/LogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fatec.livraria.repository;
-
-import com.fatec.livraria.entity.Log;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LogRepository extends JpaRepository<Log, Integer> {
-}
