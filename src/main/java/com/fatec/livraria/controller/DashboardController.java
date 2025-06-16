29d3ead1f86a48adb6f6ebff6389c7cf4bca6d6b
fabin0casa <fabiocasa999@gmail.com>
2025-06-16 00:14:22 -0300

    fix: verificando se o usuario eh admin pra enviar os dados do dashboard

--- src/main/java/com/fatec/livraria/controller/DashboardController.java
+++ src/main/java/com/fatec/livraria/controller/DashboardController.java
@@ -1,6 +1,10 @@
 package com.fatec.livraria.controller;
 
 import com.fatec.livraria.service.DashboardService;
+import com.fatec.livraria.service.PermissaoUsuarioService;
+
+import jakarta.servlet.http.HttpSession;
+
 import com.fatec.livraria.dto.response.DashboardResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -12,8 +16,12 @@ public class DashboardController {
     @Autowired
     private DashboardService dashboardService;
 
+    @Autowired
+    private PermissaoUsuarioService permissaoUsuarioService;
+
     @GetMapping
-    public DashboardResponse obterDashboard() {
+    public DashboardResponse obterDashboard(HttpSession session) {
+        permissaoUsuarioService.checarPermissaoDoUsuario(session);
         return dashboardService.getDadosDashboard();
     }
 }
